d307fa3835aac0f7305930b9f7212f92f6b7fcef
HriDev03 <hridyesh.sharma@innovasolutions.com>
2025-08-12 18:01:15 +0530

    ts functions

---
+++ functions/app.js
@@ -0,0 +1,10 @@
+console.log("TS FUNCTIONS");
+// FUNCTION RETURN TYPE
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(10, 20));
+function greet(name) {
+    console.log("Hi ".concat(name));
+}
+//1:37:48
---
+++ functions/app.ts
@@ -0,0 +1,11 @@
+console.log("TS FUNCTIONS");
+// FUNCTION RETURN TYPE
+function add(num1:number, num2:number):number{
+    return num1+num2
+}
+console.log(add(10,20));
+
+function greet(name:string):void{
+    console.log(`Hi ${name}`);
+}
+//1:37:48
